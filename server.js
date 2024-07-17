65f732e4281600710554bbe2ed3bd064e8ff5e38
Julieth-Becerra <julieth.becerra01@uptc.edu.co>
2024-07-16 19:47:02 -0500

    sdfsdf

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const routes = require('./routes');
 
 // Middleware para parsear JSON
@@ -8,6 +8,11 @@ app.use(express.json());
 
 // Usar rutas
 app.use('/', routes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
   console.log(`API funcionando en http://localhost:${port}`);
